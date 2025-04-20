adc4be6c6abf446d046e5de71ca5ff3e0a7b024d
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-04-20 21:28:45 +0600

    Postman API testing, validate form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,17 +2,47 @@
   <img src="../assets/restaurant.png" alt="Vue logo" class="logo" />
   <h3>Sign Up</h3>
   <div class="register">
-    <input type="text" placeholder="Enter Name" />
-    <input type="email" placeholder="Email" />
-    <input type="password" placeholder="Password" />
-    <button>Sign Up</button>
+    <input type="text" v-model="name" placeholder="Enter Name" />
+
+    <input type="email" v-model="email" placeholder="Email" />
+
+    <input type="password" v-model="password" placeholder="Password" />
+
+    <button @click="signUp">Sign Up</button>
+
     <p>Already have an account? <a href="#">Log in</a></p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      // Handle sign-up logic here
+      console.warn("Sign Up clicked", this.name, this.email, this.password);
+      // You can use this data to send a request to your backend API for registration
+      let result = await axios.post("http://localhost:3000/users", {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+      console.warn(result);
+      if (result.status == 201) {
+        alert("sign Up Done");
+      }
+      localStorage.setItem("user-info", JSON.stringify(result.data));
+    },
+  },
 };
 </script>
 
